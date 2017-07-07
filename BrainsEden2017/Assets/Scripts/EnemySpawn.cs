d616dc289c7d6c6cd932da1a41b02f7a239d74d0
Osama Ahmed - Source Code <osamaahmed2096@gmail.com>
2017-07-07 20:11:03 +0100

    SmokeAnimation
    Smoke Instantiates when enemies spawn

--- BrainsEden2017/Assets/Scripts/EnemySpawn.cs
+++ BrainsEden2017/Assets/Scripts/EnemySpawn.cs
@@ -5,10 +5,15 @@ public class EnemySpawn : MonoBehaviour
     public GameObject [] enemy;                
     public float spawnTime = 3f;            
     public GameObject[] spawnPoints;
+
+    public GameObject SmokeAnim;
+
+    private GameObject smoke;
     
     private GameObject spawnType;
 
     private int enemyType;
+    private int spawnPointIndex;
 
     void Start()
     {
@@ -16,16 +21,17 @@ public class EnemySpawn : MonoBehaviour
         InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
-
     void Spawn()
     {
         enemyType = Random.Range(0, 2);
 
         // Find a random index between zero and one less than the number of spawn points.
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
         // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        spawnType = (GameObject)Instantiate(enemy[enemyType], spawnPoints[spawnPointIndex].transform.position, Quaternion.identity);       
-    }
+        smoke = (GameObject)Instantiate(SmokeAnim, spawnPoints[spawnPointIndex].transform.position, Quaternion.identity);
+        spawnType = (GameObject)Instantiate(enemy[enemyType], spawnPoints[spawnPointIndex].transform.position, Quaternion.identity);
 
+        Destroy(smoke, 0.5f);  
+    }
 }
